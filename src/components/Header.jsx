ae61780691d0599f839bb30e59ce9e4cc6b4d6b6
vijay <vijay2340025@gmail.com>
2023-10-30 14:54:04 +0530

    feat: Add basic header along with icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import amazon from '../amazon_logo.png'
+import search from '../search.png'
+import '../css/Header.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <nav className="header__navbar">
+        <img src={amazon} className="header__logo" alt="logo"></img>
+        <div className='header__info'>
+            <span className='header__info__1'>Deliver to Selvaraj</span>
+            <span className='header__info__2'>Erode 638052</span>
+        </div>
+        <div className="header__search">
+            <input type='text' className='header__searchbar'></input>
+            <img src={search} className='header__searchbutton'></img>
+        </div>
+        <div className="header__rightnav">
+            <div className='header__info'>
+                <span className='header__info__1'>Hello, Vijayvignesh</span>
+                <span className='header__info__2'>Accounts & Lists</span>
+            </div>
+        </div>
+    </nav>
   )
 }
 
